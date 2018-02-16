ca6600424b7cc02df5f7c97e73e13748dfc58e39
Ivan Hladush <gladush97@gmail.com>
2018-02-16 16:43:27 +0200

    Add eureka service

--- eurekaserver/src/main/java/com/vanya/EurekaServerApplication.java
+++ eurekaserver/src/main/java/com/vanya/EurekaServerApplication.java
@@ -2,13 +2,13 @@ package com.vanya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
 @SpringBootApplication
-public class LoadApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LoadApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
--- truck/src/main/java/com/vanya/TruckApplication.java
+++ truck/src/main/java/com/vanya/TruckApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
-public class LoadApplication {
+@EnableEurekaClient
+public class TruckApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LoadApplication.class, args);
+        SpringApplication.run(TruckApplication.class, args);
     }
 }
